e5bf9766fad20669977e740eb94f1d676c038478
Douglas Squirrel <douglas.squirrel@youdevise.com>
2010-12-03 16:26:46 +0000

    second unit test fr Interpreter

--- src/test/java/com/youdevise/test/scriptwriter/InterpreterTest.java
+++ src/test/java/com/youdevise/test/scriptwriter/InterpreterTest.java
@@ -7,8 +7,10 @@ import org.junit.Test;
 
 public class InterpreterTest {
     private static final String TRIVIAL_CLASS_CODE = "public class Translator { }";
+    private static final String CLASS_NAME_FROM_TRIVIAL_CLASS = "Translator";
+    private static final String INTERFACE_CODE = "public interface Animal { }";
 
-    private Mockery context = new Mockery();    
+    private Mockery context = new Mockery();   
     
     @Test public void 
     callsStartThenFinishOnListener() {
@@ -21,15 +23,41 @@ public class InterpreterTest {
             oneOf (listener).finish(); inSequence(listenerCalls);
         }});
 
+        runAndVerifyInterpreter(listener, TRIVIAL_CLASS_CODE);
+    }
+
+    @Test
+    public void
+    givesClassNameToListener() { 
+        final TokenListener listener = context.mock(TokenListener.class);
+        final Sequence listenerCalls = context.sequence("listenerCalls");
+
+        context.checking(new Expectations() {{
+            oneOf (listener).start(); inSequence(listenerCalls);
+            oneOf (listener).giveClassName(CLASS_NAME_FROM_TRIVIAL_CLASS);
+            oneOf (listener).finish(); inSequence(listenerCalls);
+        }});
+
+        runAndVerifyInterpreter(listener, TRIVIAL_CLASS_CODE);
+    }
+
+    private void runAndVerifyInterpreter(TokenListener listener, String code) {
         Interpreter interpreter = new Interpreter(listener);
-        interpreter.interpret(TRIVIAL_CLASS_CODE);
+        interpreter.interpret(code);
 
         context.assertIsSatisfied();
     }
 
+    //@Test(expected=InterpreterException.class)
     public void
-    givesClassNameToListener() { }
+    throwsExceptionIfGivenInterface() { 
+        final TokenListener listener = context.mock(TokenListener.class);
+        final Sequence listenerCalls = context.sequence("listenerCalls");
+        
+    }
 
     public void
-    throwsExceptionForInvalidCode() { }
+    throwsExceptionIfGivenInvalidCode() { 
+
+    }
 }
